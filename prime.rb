2a12b77e10a024cf2df30241a883d4dce6cd0856
James Mobus <mobusj@live.com>
2019-08-21 01:17:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,11 @@
 # Add  code here!
 def prime?(num)
-  max = (num ** 0.5).floor
-  
-  (2..max).any? do |i|
-    num % i == 0
+  if num < 2 then
+    FALSE
+  else
+    max = (num ** 0.5).floor
+    
+    (2..max).any? do |i|
+      num % i == 0
   end
 end
